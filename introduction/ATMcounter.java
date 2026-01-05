5d6c21f8fb46e896dda8e172e25845427e0de2d6
anshgolakiya <anshgolakiya9909@gmail.com>
2026-01-05 21:32:21 +0530

    Add files via upload

---
+++ introduction/ATMcounter.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class ATMcounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the amount to withdraw: ");
+        int amount = sc.nextInt();
+        int note100 = 0, note50 = 0, note5 = 0 ,note2 = 0, note10 = 0 , note1 = 0 ;
+        note100 = amount / 100 ;
+        amount = amount % 100 ;
+
+        note50 = amount / 50 ;
+        amount = amount % 50 ;
+
+        note10 = amount / 10 ;
+        amount = amount % 10 ;
+
+        note5 = amount / 5 ;
+        amount = amount % 5 ;
+
+        note2 = amount / 2 ;
+        amount = amount % 2 ;
+
+        note1 = amount ;
+        System.out.println("Minimum number of notes : ");
+        System.out.println("Number of 100 notes: " + note100);
+        System.out.println("Number of 50 notes: " + note50);
+        System.out.println("Number of 10 notes: " + note10);
+        System.out.println("Number of 5 notes: " + note5);
+        System.out.println("Number of 2 notes: " + note2);
+        System.out.println("Number of 1 notes: " + note1);
+        sc.close();
+    }
+}
