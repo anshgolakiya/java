5ce32761a4f157fe3c1203810bc1e3d1bbc02914
anshgolakiya <anshgolakiya9909@gmail.com>
2026-01-19 11:01:06 +0530

    Add files via upload

---
+++ ansh55/ada practical/input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class input{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value of a :");
+        int a = sc.nextInt();
+        System.out.println(a);
+        sc.close();
+    }
+}
