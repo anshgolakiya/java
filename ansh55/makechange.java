587ce9555661aff9c000a1ad1e3c07742c44b37d
anshgolakiya <anshgolakiya9909@gmail.com>
2026-01-19 10:05:59 +0530

    Add files via upload

---
+++ ansh55/makechange.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class makechange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number of note you want to use : ");
+        int num = sc.nextInt();
+        int arr[] = new int[num] ;
+        int sol[] = new int[num] ;
+        for(int i = 0 ; i < num ; i++)
+        {
+            arr[i] = sc.nextInt() ;
+            sol[i] = 0 ;
+        }
+        System.out.println("Enter a amount you want to change : ");
+        int amount = sc.nextInt() ;
+        if(amount <= 0)
+        {
+            System.out.println("please enter valid amount.");
+        }
+        else
+        {
+           for(int i = 0 ; amount > 0 ; i++)
+           {
+                sol[i] = amount / arr[i] ;
+                amount = amount % arr[i] ;
+           }
+        }
+        for(int i = 0 ; i < num ; i++)
+        {
+            System.out.println(sol[i] + " ");
+        }
+        sc.close();
+    }
+}
