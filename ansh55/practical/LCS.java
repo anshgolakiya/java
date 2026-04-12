502e5c9d9e0dc6a2ae55c23c8546e6bcc50b8706
anshgolakiya <anshgolakiya9909@gmail.com>
2026-04-12 18:11:09 +0530

    Update LCS.java

--- ansh55/practical/LCS.java
+++ ansh55/practical/LCS.java
@@ -40,8 +40,8 @@ public class LCS{
             System.out.println();
         }
         System.out.println("Matrix b : ");
-        for(int i = 0 ; i <m ; i++){
-            for(int j = 0 ; j < n ; j++){
+        for(int i = 0 ; i <= m ; i++){
+            for(int j = 0 ; j <= n ; j++){
                 System.out.print(b[i][j] + " ");
             }
             System.out.println();
