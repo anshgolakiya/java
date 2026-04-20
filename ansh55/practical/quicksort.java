2c7d08e00a5dab7c8716cec1b07a491cc1b13b0f
anshgolakiya <anshgolakiya9909@gmail.com>
2026-04-20 10:32:48 +0530

    Update quicksort.java

--- ansh55/practical/quicksort.java
+++ ansh55/practical/quicksort.java
@@ -1,70 +1,75 @@
-import java.util.Scanner;
-
-public class quicksort {
-    static int pass = 1;
-
+import java.util.* ;
+public class mergesort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array : ");
         int num = sc.nextInt();
-
-        int[] arr = new int[num];
-        for (int i = 0; i < num; i++) {
-            arr[i] = (int)(Math.random() * 100);
+        int[] arr = new int[num] ;
+        for(int i = 0 ; i < num ; i++){
+            arr[i] = (int)(Math.random()*100) ;
         }
-
-        // System.out.println("Original array:");
-        // printArray(arr);
-
+        System.out.println("Given array:");
+        printArray(arr);
         long T1 = System.nanoTime();
-        quickSort(arr, 0, arr.length - 1);
+        mergeSort(arr, 0, arr.length - 1);
         long T2 = System.nanoTime();
-
-        // System.out.println("\nSorted array using quick sort:");
-        // printArray(arr);
-
-        System.out.println("\nTime taken by quick sort : " + (T2 - T1) + " nanoseconds");
+        System.out.println("\nSorted array:");
+        printArray(arr);
+        System.err.println("\nTime taken by merge sort : " + (T2 -T1) + " nanoseconds");
         sc.close();
     }
 
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-
-            
-            // System.out.print("Pass " + pass++ + ": ");
-            // printArray(arr);
-
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
         }
     }
 
-    public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
                 i++;
-                swap(arr, i, j);
+            } else {
+                arr[k] = R[j];
+                j++;
             }
+            k++;
         }
 
-        swap(arr, i + 1, high);
-        return i + 1;
-    }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
 
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
 
     public static void printArray(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
-        System.out.println();
     }
 }
