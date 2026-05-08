14c1f0e0a1be8c20a0523bb4a9c83cb0f2add558
anshgolakiya <anshgolakiya9909@gmail.com>
2026-05-08 14:06:27 +0530

    Update quicksort.java

--- ansh55/practical/quicksort.java
+++ ansh55/practical/quicksort.java
@@ -1,69 +1,60 @@
-import java.util.* ;
-public class mergesort {
+import java.util.Scanner;
+
+class QuickSort {
+
+    void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+
+                // Swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap pivot
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of array : ");
-        int num = sc.nextInt();
-        int[] arr = new int[num] ;
-        for(int i = 0 ; i < num ; i++){
-            arr[i] = (int)(Math.random()*100) ;
-        }
-        // System.out.println("Given array:");
-        // printArray(arr);
-        long T1 = System.nanoTime();
-        mergeSort(arr, 0, arr.length - 1);
-        long T2 = System.nanoTime();
-        // System.out.println("\nSorted array:");
-        // printArray(arr);
-        System.err.println("\nTime taken by merge sort : " + (T2 -T1) + " nanoseconds");
-        sc.close();
-    }
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int mid = left + (right - left) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
-    public static void merge(int[] arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[left + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[mid + 1 + j];
 
-        int i = 0, j = 0;
-        int k = left;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
+
+        QuickSort obj = new QuickSort();
+        obj.quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
-        // printArray(arr);
-    }
-    public static void printArray(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
+
+        sc.close();
     }
 }
